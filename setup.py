c9449724ad91deaa3217c458c31d482dfc49599e
Kar Sheng Lee <kar.sheng@contextlabs.com>
2023-09-01 15:08:28 +0200

    [CHORES] remove pytest

--- setup.py
+++ setup.py
@@ -4,6 +4,4 @@ setup(
     name="kane_abel_poker",
     version="0.1",
     packages=find_packages(include=["src", "pypokergui"]),
-    setup_requires=["pytest-runner"],
-    tests_require=["pytest"],
 )
