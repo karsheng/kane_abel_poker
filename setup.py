513464da5732d9e5e1f8ec381319f557f7a248e2
Kar Sheng Lee <kar.sheng@contextlabs.com>
2023-09-01 15:19:47 +0200

    [CHORES] remove version no

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name="kane_abel_poker",
     version="0.1",
-    packages=find_packages(include=["src", "pypokergui"]),
+    packages=find_packages(include=["src", "pypokergui", "pypokerengine"]),
 )
